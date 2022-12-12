70e1f12e38b7456d3bbc1f3de31d478a067c9522
Elinoa <elinoa.resnick17@gmail.com>
2022-12-12 13:56:18 +0200

    WeekMedStrong

--- src/check.js
+++ src/check.js
@@ -1,9 +1,35 @@
+import { useState, useEffect } from "react";
 
 
-function Check() {
+
+
+function Check(props) {
+  const {passLen} = props
+  const[status, setStatus] = useState()
+
+  useEffect(()=>{
+    if(passLen.length < 3){
+      setStatus("Week Password")
+    }
+    else if(passLen.length > 2 && passLen.length <7){
+      setStatus("Medium Password")
+    }
+    else if(passLen.length > 7){
+      setStatus("Strong Password")
+    }
+},[passLen])
+
+  function checkLength(){
+    if(passLen.length ===1){
+      setStatus("length is 1")
+    }
+  }
   return (
     <div className="App">
-        check
+        {/* check comp<br/> */}
+        {/* {passLen} */}
+        {status}
+
 
 
     </div>
